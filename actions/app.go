991066de77990da98a6477ab776206196be1e564
Ian Delahorne <ian.delahorne@gmail.com>
2018-02-08 19:53:42 -0800

    Proof of concept for showing flash issues
    Using a redis-backed session store won't clear flashes

--- actions/app.go
+++ actions/app.go
@@ -10,7 +10,9 @@ import (
 	"github.com/gobuffalo/buffalo/middleware/csrf"
 	"github.com/gobuffalo/buffalo/middleware/i18n"
 	"github.com/gobuffalo/packr"
-	"github.com/lflux/sessionthing/models"
+	"github.com/sirupsen/logrus"
+	"gopkg.in/boj/redistore.v1"
+	"github.com/gorilla/sessions"
 )
 
 // ENV is used to help switch settings based on where the
@@ -22,11 +24,25 @@ var T *i18n.Translator
 // App is where all routes and middleware for buffalo
 // should be defined. This is the nerve center of your
 // application.
+const useRedis = true
+
 func App() *buffalo.App {
 	if app == nil {
+		var sessionStore sessions.Store
+		if useRedis {
+			store, err := redistore.NewRediStore(10, "tcp", ":6379", "", []byte("secret"))
+			if err != nil {
+				logrus.Fatalf("Error connecting to redis: %v", err)
+			}
+			sessionStore = store
+		} else {
+			sessionStore = sessions.NewCookieStore([]byte("secret"))
+		}
+
 		app = buffalo.New(buffalo.Options{
-			Env:         ENV,
-			SessionName: "_sessionthing_session",
+			Env:          ENV,
+			SessionName:  "_sessionthing_session",
+			SessionStore: sessionStore,
 		})
 		// Automatically redirect to SSL
 		app.Use(ssl.ForceSSL(secure.Options{
@@ -42,11 +58,6 @@ func App() *buffalo.App {
 		// Remove to disable this.
 		app.Use(csrf.New)
 
-		// Wraps each request in a transaction.
-		//  c.Value("tx").(*pop.PopTransaction)
-		// Remove to disable this.
-		app.Use(middleware.PopTransaction(models.DB))
-
 		// Setup and use translations:
 		var err error
 		if T, err = i18n.New(packr.NewBox("../locales"), "en-US"); err != nil {
@@ -55,7 +66,7 @@ func App() *buffalo.App {
 		app.Use(T.Middleware())
 
 		app.GET("/", HomeHandler)
-
+		app.GET("/flash", FlashHandler)
 		app.ServeFiles("/assets", assetsBox)
 	}
 
--- actions/home.go
+++ actions/home.go
@@ -1,9 +1,17 @@
 package actions
 
-import "github.com/gobuffalo/buffalo"
+import (
+	"github.com/gobuffalo/buffalo"
+	"net/http"
+)
 
 // HomeHandler is a default handler to serve up
 // a home page.
 func HomeHandler(c buffalo.Context) error {
 	return c.Render(200, r.HTML("index.html"))
+}
+
+func FlashHandler(c buffalo.Context) error {
+	c.Flash().Add("success", "Success flash")
+	return c.Redirect(http.StatusFound, "/")
 }
